7a826fd02514e253882634ea87ec9738582bbf4f
fujiokayu <fujiokayu@gmail.com>
2020-01-26 01:41:25 -0500

    add tcp client

---
+++ chapter-02/p14_tcp-client.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import socket
+
+target_host = "www.google.com"
+target_port = 80
+
+# create socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connect to server
+client.connect((target_host,target_port))
+
+# send data
+client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+
+# receive data
+response = client.recv(4096)
+
+# convert byte data to str
+print(response.decode())
+
